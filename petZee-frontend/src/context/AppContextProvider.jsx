d1615db61a7325cf1a28f20205e59a2c4206ce80
Anish Fathima <anisfathima324@gmail.com>
2025-01-22 12:13:32 +0530

    first commit

--- petZee-frontend/src/context/AppContextProvider.jsx
+++ petZee-frontend/src/context/AppContextProvider.jsx
@@ -9,7 +9,7 @@ const AppContextProvider = ( { children } ) => {
 
     // Backend URL
 
-    const url =  'https://pet-zee-backend.onrender.com'
+    const url =  'https://petzee-backend.onrender.com'
 
     // Navbar Toggle
 
